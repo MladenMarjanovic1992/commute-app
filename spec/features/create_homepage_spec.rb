8e5b18868e03194b3fdcc77cac5da01d0d2d2e4d
Mladen Marjanovic <mladen.marjanovic1992@gmail.com>
2017-03-24 22:24:02 +0000

    create basic front page

--- spec/features/create_homepage_spec.rb
+++ spec/features/create_homepage_spec.rb
@@ -1,13 +1,15 @@
-require "rails-helper"
+require "rails_helper"
 
 RSpec.feature "Display homepage" do
-  visit "/"
-  
-  expect(page).to have_link("Commute.rs")
-  expect(page).to have_link("Sign up")
-  expect(page).to have_link("Sign in")
-  
-  expect(page).to have_content("Welcome to Commute.rs!")
-  expect(page).to have_content("Save your money and your environment by sharing a ride.")
+  scenario do
+    visit "/"
+    
+    expect(page).to have_link("Commute.rs")
+    expect(page).to have_link("Sign up")
+    expect(page).to have_link("Sign in")
+    
+    expect(page).to have_content("Welcome to Commute.rs!")
+    expect(page).to have_content("Save your money and your environment by sharing a ride.")
+  end
 
 end
