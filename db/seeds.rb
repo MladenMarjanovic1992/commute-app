93648b44005e050d3c11383e2e83a229f73aa841
Mladen Marjanovic <mladen.marjanovic1992@gmail.com>
2017-03-29 13:46:21 +0000

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,35 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+@user_names = ["mladen", "milos", "miodrag", "andrija", "nikola", "vuk", "milica", "ivana", "mila", "aleksandra"]
+@car_names = ["Corolla", "Yaris", "Verso", "Auris", "GT86", "Prius", "C-HR", "Auris TS", "Camry", "RAV4"]
+@locations = ["Belgrade", "Nis", "Novi Sad", "Vienna", "Budapest", "Athens", "Sofia", "Zagreb"]
+
+def change_to_if_same_as_from(user)
+  if user.rides.last.from == user.rides.last.to
+    Ride.update(user.id, to: (@locations - [user.rides.last.from]).sample)
+  end
+end
+
+@user_names.length.times do |num|
+  User.create!(
+    email: "#{@user_names[num - 1]}@email.com",
+    password: "password",
+    image: URI.parse("https://openclipart.org/image/2400px/svg_to_png/212896/wonderfulbeardedbaldman.png")
+  )
+end
+
+User.all.each do |user|
+  user.create_car!(
+    car_name: "Toyota #{@car_names[user.id - 1]}",
+    car_image: URI.parse("https://openclipart.org/image/2400px/svg_to_png/73711/classic-car.png")
+  )
+  user.rides.create!(
+    from: @locations.sample,
+    to: @locations.sample,
+    ride_date: rand(15).days.from_now(Date.today),
+    ride_time: Time.at(rand(86400)),
+    price: rand(400..2000).round(-2),
+    seats: rand(1..5),
+    details: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+  )
+  change_to_if_same_as_from(user)
+  
+end
